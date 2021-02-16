bbc634509328bdd601bbdeb812aab0cb9ef1d6fe
Ben Pharn <benjaminpharn@gmail.com>
2021-02-16 15:04:11 -0800

    import json and print data

--- scraper/youtube_json_parse.py
+++ scraper/youtube_json_parse.py
@@ -5,11 +5,17 @@ json_directory = 'scraper\json_files'
 
 for filename in os.listdir(json_directory):
     #  filename = os.fsdecode(file)
-     if filename.endswith(".json"): 
-         print(os.path.join(json_directory, filename))
-         continue
-     else:
-         continue
+    if filename.endswith(".json"):
+        fullpath = json_directory + "/" + str(filename)
+        print(fullpath)
+        jsonfile = open(f"{fullpath}", encoding="utf-8")
+        data = json.load(jsonfile)
+        print(data)
+        break
+        #  print(os.path.join(json_directory, filename))
+        #  continue
+    else:
+        continue
 
 
 # for x in range(42):
