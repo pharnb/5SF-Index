45ebef0a6d406ee4153737c9c375063b422d1ac2
Ben Pharn <benjaminpharn@gmail.com>
2021-02-16 15:10:21 -0800

    pretty print

--- scraper/youtube_json_parse.py
+++ scraper/youtube_json_parse.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-json_directory = 'scraper\json_files'
+json_directory = "scraper/json_files"
 
 for filename in os.listdir(json_directory):
     #  filename = os.fsdecode(file)
@@ -10,7 +10,8 @@ for filename in os.listdir(json_directory):
         print(fullpath)
         jsonfile = open(f"{fullpath}", encoding="utf-8")
         data = json.load(jsonfile)
-        print(data)
+        data_pretty = json.dumps(data, indent=2)
+        print(data_pretty)
         break
         #  print(os.path.join(json_directory, filename))
         #  continue
