4bbc7fd814aa3449d2083c33331cf487e114f3cb
Ben Pharn <benjaminpharn@gmail.com>
2021-02-18 23:13:58 -0800

    two more simple queries

--- queries.sql
+++ queries.sql
@@ -1,15 +1,18 @@
-DROP TABLE youtubedata;
-DROP TABLE imdbdata;
+-- DROP TABLE youtubedata;
+-- DROP TABLE imdbdata;
 
-CREATE TABLE youtubedata(
-	title VARCHAR (100) NOT NULL,
-	date TIMESTAMP NOT NULL,
-	description TEXT NOT NULL,
-	thumbnail VARCHAR (50) NOT NULL,
-	url VARCHAR (50) NOT NULL
-);
+-- CREATE TABLE youtubedata(
+-- 	title VARCHAR (100) NOT NULL,
+-- 	date TIMESTAMP NOT NULL,
+-- 	description TEXT NOT NULL,
+-- 	thumbnail VARCHAR (50) NOT NULL,
+-- 	url VARCHAR (50) NOT NULL
+-- );
 	
-CREATE TABLE imdbdata(
-	title VARCHAR (100) NOT NULL,
-	date TEXT NOT NULL
-);
+-- CREATE TABLE imdbdata(
+-- 	title VARCHAR (100) NOT NULL,
+-- 	date TEXT NOT NULL
+-- );
+
+SELECT * FROM public.youtubedata WHERE title = "Not Easy Being Green"
+-- SELECT * FROM public.imdbdata
