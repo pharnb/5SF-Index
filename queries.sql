4bac00f59d4c8584e0db01273049a2a0987bad18
Ben Pharn <benjaminpharn@gmail.com>
2021-02-21 20:30:42 -0800

    sql queries

--- queries.sql
+++ queries.sql
@@ -2,6 +2,7 @@
 -- DROP TABLE imdbdata;
 
 -- CREATE TABLE youtubedata(
+--  id SERIAL PRIMARY KEY,
 -- 	title VARCHAR (100) NOT NULL,
 -- 	date TIMESTAMP NOT NULL,
 -- 	description TEXT NOT NULL,
@@ -14,5 +15,22 @@
 -- 	date TEXT NOT NULL
 -- );
 
-SELECT * FROM public.youtubedata WHERE title = "Not Easy Being Green"
+SELECT * FROM dupe WHERE title = 'Mondays on CBS'
 -- SELECT * FROM public.imdbdata
+
+-- DELETE FROM youtubedata a USING youtubedata b
+-- WHERE a.id > b.id
+-- 	AND a.title = b.title;
+
+DELETE FROM dupe
+WHERE id IN
+    (SELECT id
+    FROM 
+        (SELECT id,
+         ROW_NUMBER() OVER( PARTITION BY title
+        ORDER BY  id ) AS row_num
+        FROM dupe ) t
+        WHERE t.row_num > 1 );
+		
+create table dupe as (select * from youtubedata);
+select * from dupe
